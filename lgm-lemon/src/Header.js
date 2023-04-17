e853217949620f4c0e842dd256ec3891222e01fc
aggrogahu <microgamer2vs2@yahoo.com>
2023-04-16 22:56:26 -0700

    semantics and tags

---
+++ lgm-lemon/src/Header.js
@@ -0,0 +1,13 @@
+function Header(){
+    return(
+        <>
+        Header
+        <meta name="description" content="Mediterranean Eatery - Reserve a table today"/>
+        <meta name="og:title" content="Little Lemon Restaurant"/>
+        <meta name="og:description" content="Mediterranean Eatery - Reserve a table today"/>
+        <meta name="og:image" content="img/Logo.lemon dessert.jpg"/>
+        </>
+    );
+}
+
+export default Header;
